ab01a3189ccb9fcae4a326ebee290d75636fc6f9
Danilo Giacomi <giacomi@netseven.it>
2018-02-07 17:37:46 +0100

    ASCCheckYachtNeedsMessage

---
+++ src/main/java/eu/h2020/symbiote/navigo/messages/ASCCheckYachtNeedsMessage.java
@@ -0,0 +1,28 @@
+package eu.h2020.symbiote.navigo.messages;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public class ASCCheckYachtNeedsMessage {
+
+    @JsonProperty("yachtID")
+    private String yachtID;
+
+    @JsonCreator
+    public ASCCheckYachtNeedsMessage(@JsonProperty("yacthID") String yId){
+	this.yachtID = yId;
+    }
+
+
+    @JsonProperty("yachtID")
+    public String getYachtID() {
+        return yachtID;
+    }
+    
+
+    public String toString(){
+        return "Yacht : " + yachtID ;
+    }
+}
+
+
