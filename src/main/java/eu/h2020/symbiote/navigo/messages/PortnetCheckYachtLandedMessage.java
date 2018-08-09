65bfd91807a80a2df39de6a29cd5756210891d20
Danilo Giacomi <giacomi@netseven.it>
2018-08-09 15:18:37 +0200

    PortnetCheckYachtLandedMessage

---
+++ src/main/java/eu/h2020/symbiote/navigo/messages/PortnetCheckYachtLandedMessage.java
@@ -0,0 +1,36 @@
+package eu.h2020.symbiote.navigo.messages;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public class PortnetCheckYachtLandedMessage {
+
+    @JsonProperty("yachtID")
+    private String yachtID;
+
+    @JsonProperty("callbackUrl")
+    private String callbackUrl;
+
+    @JsonCreator
+    public PortnetCheckYachtLandedMessage(@JsonProperty("yachtID") String yId, @JsonProperty("callbackUrl") String cUrl){
+        this.yachtID = yId;
+        this.callbackUrl = cUrl;
+    
+    }
+
+    @JsonProperty("yachtID")
+    public String getYachtID() {
+        return yachtID;
+    }
+    
+    @JsonProperty("callbackUrl")
+    public String getCallbackUrl() {
+        return callbackUrl;
+    }
+
+    public String toString(){
+        return "Yacht : " + yachtID ;
+    }
+}
+
+
