36a19e4b08c4be4e1fdb4d40d300f1cf278c177e
Danilo Giacomi <giacomi@netseven.it>
2018-09-05 09:05:12 +0200

    added pier and SSP IDs to PortnetWaitForYachtArrivalMessage

--- src/main/java/eu/h2020/symbiote/navigo/messages/PortnetWaitForYachtArrivalMessage.java
+++ src/main/java/eu/h2020/symbiote/navigo/messages/PortnetWaitForYachtArrivalMessage.java
@@ -7,20 +7,41 @@ public class PortnetWaitForYachtArrivalMessage {
 
     @JsonProperty("yachtID")
     private String yachtID;
+    
+    @JsonProperty("SSPID")
+    private String SSPID;
+    
+    @JsonProperty("pierID")
+    private String pierID;
 
     @JsonProperty("callbackUrl")
     private String callbackUrl;
 
     @JsonCreator
-    public PortnetWaitForYachtArrivalMessage(@JsonProperty("yachtID") String yId, @JsonProperty("callbackUrl") String cUrl){
+    public PortnetWaitForYachtArrivalMessage(
+            @JsonProperty("yachtID") String yId, 
+            @JsonProperty("callbackUrl") String cUrl,
+            @JsonProperty("SSPID") String sId,
+            @JsonProperty("pierID") String pId
+        ){
         this.yachtID = yId;
         this.callbackUrl = cUrl;
-    
+        this.SSPID = sId;
+        this.pierID = pId;
     }
 
     @JsonProperty("yachtID")
     public String getYachtID() {
         return yachtID;
+    }
+    
+    @JsonProperty("SSPID")
+    public String getSSPID() {
+        return SSPID;
+    }
+    @JsonProperty("pierID")
+    public String getPierID() {
+        return pierID;
     }
     
     @JsonProperty("callbackUrl")
